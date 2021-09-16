fa268d659d7a5295ec626242d52d0260ad5936ae
Fernando Borel <fernandomenezesborel@gmail.com>
2021-09-15 21:46:08 -0300

    Projeto Concluído

---
+++ README.md
@@ -0,0 +1,9 @@
+Jogo da velha feito em JavaScript puro.
+
+Desafio do curso de JavaScript.
+
+Cada jogador joga uma vez, o primeiro que colocar 3 "icones" iguais em linha será o vencedor. 
+
+Cronograma: Aproximadamente 3 dias.
+
+Aspectos a melhorar: Funções em JavaScript.
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,53 +1,113 @@
-// Initial Data
-
-let square = {
+// Initial data
+let quadro = {
     a1: '', a2: '', a3: '',
     b1: '', b2: '', b3: '',
     c1: '', c2: '', c3: ''
 };
-
-let player = '';
-let warning = '';
 let playing = false;
+let vez = 'x';
+let warning = '';
 
-// Events
+reset();
 
+// Event
 document.querySelector('.reset').addEventListener('click', reset);
 
-// Functions
+document.querySelectorAll('.item').forEach((item)=>{
+    item.addEventListener('click', (e) => {
+        let loc = e.target.getAttribute('data-item');
+        
+        if(playing && quadro[loc] === '') {
+            quadro[loc] = vez;
+            renderQuadro();
+            togglePlayer();
+        }
+    });
+});
 
+// Function
 function reset() {
     warning = '';
 
-    let random = Math.floor(Mathe.random() *2);
-    player = (random === 0) ? 'x' : 'o';
+    // playing
+    let random = Math.floor(Math.random() * 2);
+    vez = random === 0 ? 'x' : 'o';
 
-    for(let i in square) {
-        square[i] = '';
+    // reset square
+    for(let i in quadro) {
+        quadro[i] = '';
     }
 
-    playing = true;
-    
-    renderSquare();
+    // render
+    renderQuadro();
     renderInfo();
+
+    playing = true;
 }
 
-//
-
-function renderSquare() {
-    for (let i in square) {
+function renderQuadro() {
+    for(let i in quadro) {
         let item = document.querySelector(`div[data-item=${i}]`);
-        if(square[i] !== '') {
-            item.innerHTML = square[i];
+        if(quadro[i] !== '') {
+            item.innerHTML = quadro[i];
         } else {
             item.innerHTML = '';
         }
     }
+
+    checkGame();
 }
 
-//
-
 function renderInfo() {
-    document.querySelector('.vez').innerHTML = player;
+    document.querySelector('.vez').innerHTML = vez;
     document.querySelector('.resultado').innerHTML = warning;
+}
+
+function togglePlayer() {
+    vez = vez === 'x' ? 'o' : 'x';
+    renderInfo();
+}
+
+function checkGame() {
+    if(checkWinnerFor('x')) {
+        warning = 'O "x" venceu';
+        playing = false;
+    } else if(checkWinnerFor('o')) {
+        warning = 'O "o" venceu';
+        playing = false;
+    } else if(isFull()) {
+        warning = 'Deu empate';
+        playing = false;
+    }
+}
+
+function checkWinnerFor(i) {
+    let pos = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1'
+    ];
+
+    for(let w in pos) {
+        let pArray = pos[w].split(',');
+        let hasWon = pArray.every(option=>quadro[option] === i);
+        if(hasWon) return true;
+    }
+
+    return false;
+}
+function isFull() {
+    for(let i in quadro) {
+        if(quadro[i] === '') {
+            return false;
+        }
+    }
+    return true;
 }
